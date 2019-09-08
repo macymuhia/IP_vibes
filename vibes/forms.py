ea0eaa81f29f30999c9918e8984b4899f14d82ec
macymuhia <macymuhia@gmail.com>
2019-09-08 21:46:02 +0300

    Add a userform for the fields that can be edited in the profile

--- vibes/forms.py
+++ vibes/forms.py
@@ -11,3 +11,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email']
