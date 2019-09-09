9013c9604a755bbfa6dcf6459d4d2add0548e3cf
macymuhia <macymuhia@gmail.com>
2019-09-09 10:13:16 +0300

    Add a url to add a new project

--- vibes/urls.py
+++ vibes/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
          name='account_activation_sent'),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             views.activate, name='activate'),
+    path("new_project", views.new_project, name="new_project")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
